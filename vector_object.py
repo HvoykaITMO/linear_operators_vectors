a11823f4d90caf3e1382d376dd29079221d45280
HvoykaITMO <465503@niuitmo.ru>
2025-06-25 02:07:15 +0300

    Added an endomorphism hierarchy panel in the vector properties panel. The properties of the vector have been redesigned (their display has not changed). Added new operators for interacting with the hierarchy panel. A new feature has been added that forcibly updates the interface. Added auxiliary functions required for the endomorphism hierarchy panel

--- vector_object.py
+++ vector_object.py
@@ -1,20 +1,28 @@
 import bpy
-from bpy.props import FloatVectorProperty, BoolProperty, EnumProperty, PointerProperty
+from bpy.props import FloatVectorProperty, BoolProperty, EnumProperty, PointerProperty, StringProperty, IntProperty
 from bpy.types import Context, Event
 from typing import Literal
 import mathutils
 
 
+# Принудительная перерисовка интерфейса.
+def force_ui_redraw(context):
+    for window in context.window_manager.windows:
+        for area in window.screen.areas:
+            if area.type == 'VIEW_3D':
+                area.tag_redraw()
+
+
 # Функция для изменения координат вектора при их изменении в панели свойств.
 def update_vector_end(self, context):
-    obj = self.id_data  # объект, к которому привязаны свойства
+    obj = self.id_data  # объект, к которому привязаны свойства.
 
-    # Обновляем положение точки
+    # Обновляем положение точки.
     if self.vector_type == 'POINT':
         if obj.type == 'MESH':
             mesh = obj.data
             mesh.vertices[0].co = self.vector_end
-    # Обновляем направление и длину стрелки
+    # Обновляем направление и длину стрелки.
     elif self.vector_type == 'ARROW':
         obj.location = (0, 0, 0)
         vec = mathutils.Vector(self.vector_end)
@@ -44,7 +52,22 @@ def update_all_vectors(scene):
                 obj.scale = (1, 1, vec.length)
 
 
-# Класс с кастомными свойствами
+def get_operator_items(self, context):
+    items = []
+    for obj in bpy.data.objects:
+        if hasattr(obj, "endomorphism_props") and obj.endomorphism_props.uuid:
+            # В качестве значения используем UUID, а в качестве текста — имя объекта
+            items.append((obj.endomorphism_props.uuid, obj.name, ""))
+    return items
+
+
+# Структура одного из элементов коллекции (applied_endomorphisms)
+class VectorAppliedEndomorphism(bpy.types.PropertyGroup):
+    endomorphism_uuid: bpy.props.StringProperty(
+        name="Endomorphism UUID"
+    )
+
+
 class VectorProperties(bpy.types.PropertyGroup):
     is_vector: bpy.props.BoolProperty(
         name="Is Vector object", default=False
@@ -62,6 +85,9 @@ class VectorProperties(bpy.types.PropertyGroup):
         items=[('POINT', "Point", ""), ('ARROW', "Arrow", "")],
         default='POINT'
     )
+    applied_endomorphisms: bpy.props.CollectionProperty(
+        type=VectorAppliedEndomorphism)
+    applied_endomorphisms_index: bpy.props.IntProperty(default=-1)
 
 
 class AddVectorObject(bpy.types.Operator):
@@ -120,6 +146,94 @@ class AddVectorObject(bpy.types.Operator):
         return context.window_manager.invoke_props_dialog(self)
 
 
+class VECTOR_OT_add_endomorphism(bpy.types.Operator):
+    bl_idname = "vector.add_endomorphism"
+    bl_label = "Add Endomorphism"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    endomorphism_uuid: bpy.props.EnumProperty(
+        name="Endomorphism",
+        description="Choose an Endomorphism",
+        items=get_operator_items
+    )
+
+    def execute(self, context):
+        vector_props = context.object.vector_props
+        # Добавляем пустой элемент и получаем ссылку на него
+        new_op = vector_props.applied_endomorphisms.add()
+        new_op.endomorphism_uuid = self.endomorphism_uuid  # Заполняем элемент
+        vector_props.applied_endomorphisms_index = len(
+            vector_props.applied_endomorphisms) - 1
+
+        force_ui_redraw(context)
+        return {'FINISHED'}
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "endomorphism_uuid")
+
+    def invoke(self, context, event):
+        # Если операторов нет — сразу завершить
+        if not get_operator_items(self, context):
+            self.report({'WARNING'}, "No Endomorphisms in the scene")
+            return {'CANCELLED'}
+        return context.window_manager.invoke_props_dialog(self)
+
+
+class VECTOR_OT_remove_endomorphism(bpy.types.Operator):
+    bl_idname = "vector.remove_endomorphism"
+    bl_label = "Remove Endomorphism"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        vector_props = context.object.vector_props
+        index = vector_props.applied_endomorphisms_index
+        if index >= 0:
+            vector_props.applied_endomorphisms.remove(index)
+            vector_props.applied_endomorphisms_index = min(
+                index, len(vector_props.applied_endomorphisms) - 1)
+
+        force_ui_redraw(context)
+        return {'FINISHED'}
+
+
+class VECTOR_OT_move_endomorphism(bpy.types.Operator):
+    bl_idname = "vector.move_endomorphism"
+    bl_label = "Move Endomorphism"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    direction: bpy.props.EnumProperty(
+        items=[('UP', 'Up', ''), ('DOWN', 'Down', '')])
+
+    def execute(self, context):
+        vector_props = context.object.vector_props
+        index = vector_props.applied_endomorphisms_index
+        new_index = index + (1 if self.direction == 'DOWN' else -1)
+
+        if 0 <= new_index < len(vector_props.applied_endomorphisms):
+            vector_props.applied_endomorphisms.move(index, new_index)
+            vector_props.applied_endomorphisms_index = new_index
+
+        force_ui_redraw(context)
+        return {'FINISHED'}
+
+
+# UIList для управления порядком операторов в панели свойств у векторов
+class VECTOR_UL_endomorphisms_list(bpy.types.UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index=0, flt_flag=0):
+        endomorphism_uuid = item.endomorphism_uuid
+
+        # Выбираем подходящие эндоморфизмы. Если несколько - берём первый.
+        endomorphism_obj = next((obj for obj in bpy.data.objects
+                                 if hasattr(obj, "endomorphism_props") and
+                                 obj.endomorphism_props.uuid == endomorphism_uuid), None)
+
+        if endomorphism_obj:
+            layout.label(text=endomorphism_obj.name)
+        else:
+            layout.label(text="Unknown Endomorphism")
+
+
 # Панель, которая показывает свойства только для векторов
 class Vector_PT_Panel(bpy.types.Panel):
     bl_label = "Properties"
@@ -136,7 +250,7 @@ class Vector_PT_Panel(bpy.types.Panel):
         # Имеет ли он атрибут vector_props (например, у старых объектов сцены их не будет, как и у некоторых "нетипичных" объектов)
         # Вектора ли это (по флажку is_vector)
         is_great = obj is not None and hasattr(
-            obj, "vector_props") and obj.vector_props.is_vector  # type: ignore
+            obj, "vector_props") and getattr(obj.vector_props, "is_vector", False)  # type: ignore
         return is_great
 
     def draw(self, context):
@@ -152,6 +266,26 @@ class Vector_PT_Panel(bpy.types.Panel):
         col.prop(props, "vector_end", index=1, text="Y")
         col.prop(props, "vector_end", index=2, text="Z")
 
+        # Далее список иерархии эндоморфизмов
+        layout.separator()
+        layout.label(text="Applied Endomorphisms:")
+        row = layout.row()
+        row.template_list(
+            "VECTOR_UL_endomorphisms_list",
+            "",
+            context.object.vector_props,
+            "applied_endomorphisms",
+            context.object.vector_props,
+            "applied_endomorphisms_index"
+        )
+        col = row.column(align=True)
+        col.operator("vector.add_endomorphism", icon='ADD', text="")
+        col.operator("vector.remove_endomorphism", icon='REMOVE', text="")
+        col.operator("vector.move_endomorphism", icon='TRIA_UP',
+                     text="").direction = 'UP'
+        col.operator("vector.move_endomorphism", icon='TRIA_DOWN',
+                     text="").direction = 'DOWN'
+
 
 # Отображение нового типа в панели создания объектов (shift + A)
 def menu_func(self, context):
@@ -160,6 +294,11 @@ def menu_func(self, context):
 
 
 def register():
+    bpy.utils.register_class(VECTOR_UL_endomorphisms_list)
+    bpy.utils.register_class(VECTOR_OT_add_endomorphism)
+    bpy.utils.register_class(VECTOR_OT_remove_endomorphism)
+    bpy.utils.register_class(VECTOR_OT_move_endomorphism)
+    bpy.utils.register_class(VectorAppliedEndomorphism)
     bpy.utils.register_class(VectorProperties)
     bpy.utils.register_class(AddVectorObject)
     bpy.utils.register_class(Vector_PT_Panel)
@@ -171,6 +310,11 @@ def register():
 
 def unregister():
     bpy.types.VIEW3D_MT_mesh_add.remove(menu_func)
+    bpy.utils.unregister_class(VECTOR_UL_endomorphisms_list)
+    bpy.utils.unregister_class(VECTOR_OT_add_endomorphism)
+    bpy.utils.unregister_class(VECTOR_OT_remove_endomorphism)
+    bpy.utils.unregister_class(VECTOR_OT_move_endomorphism)
+    bpy.utils.unregister_class(VectorAppliedEndomorphism)
     bpy.utils.unregister_class(Vector_PT_Panel)
     bpy.utils.unregister_class(AddVectorObject)
     bpy.utils.unregister_class(VectorProperties)
